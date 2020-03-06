e89a1f59ad59cda147078555c29624a670d784b3
Sebastian Delima <sebastiandelima@Cactus1.local>
2020-03-05 18:12:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// Code your solution here!
+function printString(myString){
 
+    console.log(myString[0])
+
+    if(myString.length > 1){
+      let newString = myString.substring(1, myString.length )
+      printString(newString)
+    } else {
+        return true
+    }
+}
+
+function reverseString(){
+    
+}
